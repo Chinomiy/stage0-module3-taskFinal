48d50b37db561bf913a7b4aa9dc85566a6b103ae
Chinomiy <maruishiwo@gmail.com>
2023-10-12 20:21:19 +0300

    v2

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+
+        for(int i; number != 0; number = number / 10) {
+
+            i = number % 10;
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        StringBuilder reverseNumber = new StringBuilder();
+        for(int i; number != 0; number /= 10) {
+            i = number % 10;
+            reverseNumber.append(i);
+        }
+        System.out.println(reverseNumber);
     }
 }
